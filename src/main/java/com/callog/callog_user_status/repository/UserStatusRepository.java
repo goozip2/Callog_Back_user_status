2b4cd8629fc444190f49b0c2b53519b64a74ef7a
suyeon <93810963+syjungsuyeon@users.noreply.github.com>
2025-06-23 18:07:13 +0900

    Update Repository
    fix: Update UserStatus, WeightInfo Repository

--- src/main/java/com/callog/callog_user_status/repository/UserStatusRepository.java
+++ src/main/java/com/callog/callog_user_status/repository/UserStatusRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface UserStatusRepository extends JpaRepository<UserStatus, Long> {
-
     Optional<UserStatus> findByUserId(String userId);
 }
--- src/main/java/com/callog/callog_user_status/repository/WeightInfoRepository.java
+++ src/main/java/com/callog/callog_user_status/repository/WeightInfoRepository.java
@@ -2,7 +2,6 @@ package com.callog.callog_user_status.repository;
 
 import com.callog.callog_user_status.domain.WeightInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
