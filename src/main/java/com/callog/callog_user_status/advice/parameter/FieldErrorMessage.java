7a84d96d800cb4d7e14ab40b6ed76a729c531563
suyeon <93810963+syjungsuyeon@users.noreply.github.com>
2025-06-24 15:16:29 +0900

    feat: add Error Parameter
    feat: add Error Parameter

---
+++ src/main/java/com/callog/callog_user_status/advice/parameter/FieldErrorMessage.java
@@ -0,0 +1,19 @@
+package com.callog.callog_user_status.advice.parameter;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class FieldErrorMessage {
+    private String field;
+    private String message;
+
+    @Builder
+    public FieldErrorMessage(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+}
+
